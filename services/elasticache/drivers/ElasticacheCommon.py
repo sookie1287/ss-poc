e4e26e7fd1dbd7601a5f7f7a9cc982778688df1a
Kitt Choy <kitt.dmz@gmail.com>
2023-04-18 11:59:00 +0800

    added encryption check, cleanup reporter messages

--- services/elasticache/drivers/ElasticacheCommon.py
+++ services/elasticache/drivers/ElasticacheCommon.py
@@ -6,4 +6,13 @@ class ElasticacheCommon(Evaluator):
         super().__init__()
         self.cluster = cluster
         self.client = client
-    
+
+    def _checkEncryption(self):
+        stringBuild = []
+        if self.cluster.get('TransitEncryptionEnabled') is not True:
+            stringBuild.append('in transit')
+        if self.cluster.get('AtRestEncryptionEnabled') is not True:
+            stringBuild.append('at rest')
+        
+        if len(stringBuild) > 0:
+            self.results['EncInTransitAndRest'] = [-1, f"Not using encryption {' and '.join(stringBuild)}"]
